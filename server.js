dd01bd0d09f05e342bf7e8cb738b21a8821ed5fb
PaulGelderloos <paul@gelderloos.com>
2026-02-03 09:40:18 +0100

    Update server.js

--- server.js
+++ server.js
@@ -32,4 +32,4 @@ app.post("/webhook", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server draait op poort ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server draait op poort ${PORT}`));
