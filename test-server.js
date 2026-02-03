eacdf45c278bf883033e26e08689918c19896033
PaulGelderloos <paul@gelderloos.com>
2026-02-03 10:06:22 +0100

    Create test-server.js

---
+++ test-server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const PORT = process.env.PORT || 8080;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('OK');
+});
+
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Test server draait op ${PORT}`);
+});
